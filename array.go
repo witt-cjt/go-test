e9d23aff77181ca08646d3cbfa054eb06d34b76a
Chu Ji Tao <chujitao@gmail.com>
2016-08-18 21:48:44 +0800

    first commit

---
+++ array.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Currency int
+
+const (
+	USD Currency = iota // 美元
+	EUR                 // 欧元
+	GBP                 // 英镑
+	RMB                 // 人民币
+)
+
+func main() {
+	fmt.Println(time.Now())
+	symbol := [...]string{USD: "$", EUR: "€", GBP: "￡", RMB: "￥"}
+	fmt.Println(RMB, symbol[RMB]) // "3 ￥"
+}
